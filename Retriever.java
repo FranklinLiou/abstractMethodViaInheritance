f95888c12591a5e7c9008c2b7ac6241d63e80c48
Saad Bhuiyan <ssaad00@stuy.edu>
2019-03-01 13:06:36 -0500

    Saad's Changes

--- Retriever.java
+++ Retriever.java
@@ -1,3 +1,4 @@
+// Retriever's make more of a "roof" sound than "arf." - Saad
 public class Retriever extends Pet{
     public String speak() {
         return "arf";
--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -1,3 +1,4 @@
+// Saad likes Franklin's dog's name.
 public class UserOfSpeakers {
     public static void main(String[] args) {
         Animal gracie = new Retriever();
